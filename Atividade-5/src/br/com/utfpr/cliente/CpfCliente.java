442e82938f0fab5cab366afdeefefe39a2d37c00
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-02-17 21:28:45 -0300

    Entrega da atividade 5

--- Atividade-5/src/br/com/utfpr/cliente/CpfCliente.java
+++ Atividade-5/src/br/com/utfpr/cliente/CpfCliente.java
@@ -1,6 +1,7 @@
 package br.com.utfpr.cliente;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.util.Scanner;
 
 import br.com.utfpr.service.CpfService;
@@ -9,17 +10,17 @@ public class CpfCliente {
 
 	public static void main(String[] args) {
 		
+		Scanner in = null;
+		
 		int opcao = -1;
 		
 		try {
 			CpfService cpfService = (CpfService) Naming.lookup("rmi://localhost:1099/CpfService");
 			
-			Scanner in = new Scanner(System.in);
-			System.out.println("Selecione uma opção: ");
-			System.out.println("1 -> Validar CPF");
-			System.out.println("2 -> Informar a quantidade de CPF's validados (Não implementado ainda)");
-			System.out.print("Opção: ");
+			in = new Scanner(System.in);
 			do {
+				exibirMenuPrincipal();
+				System.out.print("\nOpção: ");
 				try {
 					opcao = in.nextInt();
 					in.nextLine();
@@ -27,30 +28,47 @@ public class CpfCliente {
 					System.out.println("A opção informada deve ser um número.");
 				}
 				
-				switch (opcao) {
-				case 1:
-					System.out.println("Informe um CPF para ser validado (incluir ponto(.) e traço separador de digito (-) ex.: XXX.XXX.XXX-XX): ");
-					System.out.print("CPF: ");
-					String cpf = in.nextLine();
-					System.out.println(cpfService.validaCpf(cpf));
-					break;
-				case 2:
-					System.out.printf("Número de CPF's válidos: %s\n", cpfService.getQuantidadeCpfsValidos());
-					break;
-				default:
-					break;
-				}
-				
-				
-				if (opcao < 0 || opcao > 2) {
+				tratarOpcao(in, opcao, cpfService);
+				if (opcao < 0 || opcao > 3) {
 					System.out.println("Opção inválida. Informe selecione uma opção válida");
-					break;
 				}
 				
-			} while (opcao < 0 || opcao > 2);
+			} while (opcao != 3);
 			
 		} catch (Exception e) {
 			System.out.printf("Erro RMI: %s", e.getMessage());
+		} finally {
+			if (in != null) {
+				in.close();
+			}
 		}
+	}
+
+	private static void tratarOpcao(Scanner in, int opcao, CpfService cpfService) throws RemoteException, InterruptedException {
+		switch (opcao) {
+		case 1:
+			System.out.println("\nInforme um CPF para ser validado (incluir ponto(.) e traço separador de digito (-) ex.: XXX.XXX.XXX-XX): ");
+			System.out.print("CPF: ");
+			String cpf = in.nextLine();
+			System.out.println("\n" + cpfService.validaCpf(cpf));
+			break;
+		case 2:
+			System.out.printf("\nNúmero de CPF's válidos: %s\n", cpfService.getQuantidadeCpfsValidos());
+			break;
+		case 3:
+			System.out.println("\nFechando o cliente...");
+			break;
+		default:
+			break;
+		}
+		
+		Thread.sleep(1000);
+	}
+
+	private static void exibirMenuPrincipal() {
+		System.out.println("\nSelecione uma opção: ");
+		System.out.println("1 -> Validar CPF");
+		System.out.println("2 -> Informar a quantidade de CPF's validados (Não implementado ainda)");
+		System.out.println("3 -> Sair");
 	}
 }
